118506bcb95f043c14fc758485166da2863c224c
dandeville <dandeville26@gmail.com>
2022-04-15 15:28:06 -0500

    Commit Inicial

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Card = ({ name, email, id }) => {
+  return (
+    <aside>
+      <h1></h1>
+    </aside>
+  )
+}
+
+export default Card
---
+++ src/containers/App.js
@@ -0,0 +1,10 @@
+
+const App = () =>{
+  return (
+    <div className="App">
+      <h1>Hola</h1>
+    </div>
+  )
+}
+
+export default App;
